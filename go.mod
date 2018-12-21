04876aca46de74c52738dacaacd4218709cf0bdf
Russ Magee <rmagee@gmail.com>
2018-12-20 18:59:08 -0800

    Project creation
    Signed-off-by: Russ Magee <rmagee@gmail.com>

---
+++ go.mod
@@ -0,0 +1,1 @@
+module blitter.com/go/gowebhook
---
+++ gowebhook.go
@@ -0,0 +1,13 @@
+// Package gowebhook is a Webhook listener that dispatches
+// arbitrary commands on receipt of webhook events.
+// Supported webhook event formats: gogs, (.. future)
+//
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
