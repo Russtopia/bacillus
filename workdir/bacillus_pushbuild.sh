741129b6deb2e51c7de31b9c721f95a8bb7e34a4
Russ Magee <rmagee@gmail.com>
2019-01-24 20:12:14 -0800

    Added new bacillus_pushbuild.sh script
    Signed-off-by: Russ Magee <rmagee@gmail.com>

---
+++ workdir/bacillus_pushbuild.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export PATH=/usr/local/bin:/usr/bin:/usr/lib/ccache/bin:/bin
+echo "workdir: ${BACILLUS_WORKDIR}"
+mkdir -p "${BACILLUS_ARTFDIR}"
+
+export GO111MODULE=auto
+export GOPATH="${HOME}/go"
+# GOCACHE will be phased out in v1.12. [github.com/golang/go/issues/26809]
+export GOCACHE="${HOME}/.cache/go-build"
+
+echo "---"
+go env
+echo "---"
+echo "passed env:"
+env
+echo "---"
+
+#cd ${BACILLUS_WORKDIR}
+echo "curDir: $PWD"
+rm -rf build
+mkdir -p build
+cd build
+git clone https://gogs.blitter.com/Russtopia/bacillus
+cd bacillus
+
+go build .
+
+echo "--Done--"
