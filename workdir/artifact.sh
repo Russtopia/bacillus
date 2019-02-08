e7c660a82224f3cf0b1f0be350717b08e557f2da
Russ Magee <rmagee@gmail.com>
2019-02-07 23:28:04 -0800

    A very provisional scheme for 'build with parameters'
    Signed-off-by: Russ Magee <rmagee@gmail.com>

--- workdir/artifact.sh
+++ workdir/artifact.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# TODO: provisional 'build with params' notation
+# types: s - string, freeform
+#        c - choice (one-of), string
+#        b - boolean (true/false)
+#        
+# ?s?DELAY?5
+# ?c?SUITE?small|big|huge
+# ?b?DEBUG?false
+#
+# all params are strings, so REST calls can encode as POST params
+# eg., ".../some_job/?DELAY=5&SUITE=big&DEBUG=true
+#  (these would override defaults first parsed from this file above, or set below)
+DELAY=${DELAY:-"5"}
+SUITE=${SUITE:-"small"}
+DEBUG=${DEBUG:-"false"}
+##
 
 delay=${1:-"5"}
 
