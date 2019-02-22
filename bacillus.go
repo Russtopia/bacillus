4f1039ebeed85151a840aaf4a231e57798305008
Russ Magee <rmagee@gmail.com>
2019-02-22 00:10:45 -0800

    Fixed wording for /shutdown link
    Signed-off-by: Russ Magee <rmagee@gmail.com>

--- bacillus.go
+++ bacillus.go
@@ -960,7 +960,7 @@ Latest Job Activity (Running jobs:<span id='liveRunLogCount'>`+fmt.Sprintf("%d",
      Happy Build Automating, DevOps-ing, or whatever it's called these days...
 	 
   Oh, and in case you need to...
-  <a href='/shutdown'>halt any new jobs for a graceful shutdown</a>   (afterwards, use <strong>/rudeshutdown</strong>)
+  <a href='/shutdown'>prevent any new jobs for a graceful shutdown</a>   (afterwards, use <strong>/rudeshutdown</strong>)
   <a href='/cancelshutdown'>cancel a planned shutdown</a>
 `)
 	if basicAuth {
