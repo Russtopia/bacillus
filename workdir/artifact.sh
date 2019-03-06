9c8b360820758382d079773f606af4719ecde681
Russ Magee <rmagee@gmail.com>
2019-03-06 14:24:59 -0800

    Added stages to artifact.sh
    Signed-off-by: Russ Magee <rmagee@gmail.com>

--- workdir/artifact.sh
+++ workdir/artifact.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# TODO: provisional 'build with params' notation
+# Parameterized build syntax
+# --
 # types: s - string, freeform
 #        c - choice (one-of), string
 #        b - boolean (true/false)
@@ -9,27 +10,44 @@
 #-?c?SUITE?small|big|huge?heap size
 #-?b?DEBUG?1?Keep debug symbols
 #-?b?Detonator?disabled
+# --
 #
 # all params are strings, so REST calls can encode as POST params
 # eg., ".../some_job/?DELAY=5&SUITE=big&DEBUG=true
 #  (these would override defaults first parsed from this file above, or set below)
+#
+# Defaults
+# --
 DELAY=${DELAY:-"5"}
 SUITE=${SUITE:-"small"}
 DEBUG=${DEBUG:-"false"}
 ##
 
+function stage {
+  local _stage="${BACILLUS_WORKDIR}"/_stage
+  
+  if [ ! -f ${_stage} ]; then
+    echo -n "$1" >"${BACILLUS_WORKDIR}"/_stage
+  else
+    echo -n ":$1" >>"${BACILLUS_WORKDIR}"/_stage
+  fi
+}
+
 delay=${1:-"5"}
 
+stage "Setup"
 echo "workdir: ${PWD}"
 env
 
-ADIR="${BACILLUS_ARTFDIR}"
-mkdir -p "${ADIR}"
-echo "blah" >"${ADIR}/artifact.txt"
-
+stage "Busywork"
 for i in $(seq 1 4); do
   echo "Doing some work (sleeping ${delay}). $i ..."
   sleep ${delay}
 done
+
+stage "Artifacts"
+ADIR="${BACILLUS_ARTFDIR}"
+mkdir -p "${ADIR}"
+echo "blah" >"${ADIR}/artifact.txt"
 
 echo "--DONE--"
